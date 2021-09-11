9c5c1e33a6de10b43de5771fcffce22c8b394c91
dangcongkhoi <57400197+dangcongkhoi@users.noreply.github.com>
2021-09-11 20:44:06 +0700

    add Pause Button

---
+++ Assets/Assets/PauseGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public void Restart()
+    {
+        Debug.Log("helloWord");
+        GridManager.Instance.GameOver();
+        
+    }
+}
